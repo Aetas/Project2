57667ce9d50fd8c6fdee0ee35427edfc4f9a5124
Derek Prince <ace.prince08@gmail.com>
2015-01-24 11:42:46 -0700

    Added initial source (Assignment2.cpp) file to master.

---
+++ Assignment2.cpp
@@ -0,0 +1,28 @@
+//
+// Derek Prince
+// CSCI 2270 Assignment 2
+// Hemingway's 350 stalkers
+//
+//
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<sstream>
+
+/*
+ create blaclist string blacklist[100] to step through and check?
+probably have to.
+*/
+
+using std::endl;
+
+
+int main()
+{
+	
+	
+	cout << endl
+		<< endl;
+	return 0;
+}
