254ebec33c070769e822a4fa4a278681ff64c99a
Derek Prince <ace.prince08@gmail.com>
2015-01-29 17:42:34 -0700

    Cleaned up program and added comments to most significant lines. Also changed some old lines to keep current with the newer styles and methods. removed cstdlib as well. Didnt need it

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,12 +1,12 @@
-//
-// Derek Prince
-// CSCI 2270 Assignment 2
-// Hemingway's 350 stalkers
-//
-//
+/* 
+ * Derek Prince
+ * CSCI 2270 
+ * Rhonda Hoenigman
+ * Assignment 2 - Hemingway's 350 stalkers
+ *
+*/
 
 #include<iostream>
-#include<cstdlib>
 #include<string>
 #include<fstream>
 
@@ -27,11 +27,11 @@ struct blacklist	//because why not, now i have a constructor
 	string list[50];
 };
 
-void pull(std::ifstream &in_doc, wordbank bank[]);
-wordbank* double_array(wordbank bank[]);
-void commit(wordbank bank[], string &buffer);
-bool if_commons(string &buffer);
-void sort(wordbank bank[]);
+wordbank* double_array(wordbank bank[]);			//doubles the array size
+void pull(std::ifstream &in_doc, wordbank bank[]);	//pulls out the data. Wound up being useless.
+void commit(wordbank bank[], string &buffer);		//responsible for commiting new entries in the array and in the correct place
+void sort(wordbank bank[]);							//currently just a simple shitty bubble sort. meh.
+bool if_commons(string &buffer);					//basically just loop control to see if things are common words
 
 blacklist nay;		//nay tarry 'bout these words. or that there are 50 global strings floating about.
 int size = 100;		//for doubling and book keeping
@@ -43,8 +43,7 @@ int main()
 {
 	string document;	//doc-to-be
 	int print_num;		//number of top words to print
-	cin >> document >> print_num;
-	print_num = 10;
+	cin >> document >> print_num;	//simple cin will work since they are spaced correctly	
 
 	std::ifstream in_doc;	//make ifstream
 	in_doc.open(document);	//open file
@@ -53,142 +52,151 @@ int main()
 	
 	if (in_doc.is_open())	//check if it opened successfully
 	{
-		string buffer;
-
+		string buffer;	//simple string to work with
 		while (in_doc >> buffer)
 		{
-//			cout << " pull";
-			if (if_commons(buffer) == true)
-			{/*nothing*/}
+			if (if_commons(buffer) == true)	//if it matches, don't do anything
+			{/*nothing*/
+			}
 			else
 			{
-				commit(bank, buffer);
+				commit(bank, buffer);	//otherwise commit it to the array
 			}
-			if (made == size - 1)
+			if (made == size - 1)		//if the array is at the end, double it
 			{
-				bank = double_array(bank);
-				doubled++;
+				bank = double_array(bank);	//double_array returns a pointer of wordbank type, so it can be used with the assignment operator
+				doubled++;					//book keeping
 			}
 		}
-		in_doc.close();	//close doc after done
+		in_doc.close();		//close doc after done
 	}
-/*	else	//otherwise it didnt open
+	else	//otherwise it didnt open
 	{
 		while (!in_doc.is_open())	//query reoeatedly until file is found.
 		{
 			cout << "\n Document not found"
-				<< "\n <name.txt>: ";
-			getline(cin, document);
+			<< "\n <name.txt>: ";
+			cin >> document;		//grab document -- number of lines was already stored
 			in_doc.open(document);	//open again to update contitions
 		}
-		pull(in_doc, &bank);		//pull words
-		in_doc.close();				//close after end
-	}*/
+		string buffer;	//simple string to work with
+		while (in_doc >> buffer)
+		{
+			if (if_commons(buffer) == true)	//if it matches, don't do anything
+			{/*nothing*/
+			}
+			else
+			{
+				commit(bank, buffer);	//otherwise commit it to the array
+			}
+			if (made == size - 1)		//if the array is at the end, double it
+			{
+				bank = double_array(bank);	//double_array returns a pointer of wordbank type, so it can be used with the assignment operator
+				doubled++;					//book keeping
+			}
+		}
+		in_doc.close();			//close after end
+	}
 
 	//print stuff.
-	sort(bank);
-	for (int i = 0; i < print_num; i++)
+	sort(bank);	//call simple sort on array
+	for (int i = 0; i < print_num; i++)	//print the top entries, braked on the input
 	{
 		cout << bank[i].count << " - " << bank[i].word << endl;
 		cout << "#" << endl;
 	}
-	cout << "Array doubled: " << doubled << endl;
+	cout << "Array doubled: " << doubled << endl;	//just some stats n stuff
 	cout << "#" << endl;
 	cout << "Unique non-common words: " << made << endl;
 	cout << "#" << endl;
 	cout << "Total non-common words: " << total << endl;
 
-	cout << endl
+	cout << endl	//this might throw off the cog grader, we shall see
 		<< endl;
 	return 0;
 }
 
-/*void pull(std::ifstream &in_doc, wordbank *bank[])	//grab words.
+void pull(std::ifstream &in_doc, wordbank bank[])	//grab words. doesn't actually work as it turns out
 {
-	string buffer;
-	int position = 0;
-	while (getline(in_doc, buffer, ' '))
+	string buffer;	//simple string to work with
+	while (in_doc >> buffer)
 	{
-		if (if_commons(buffer, bank))
-		{/*nothing*//*}
+		if (if_commons(buffer) == true)	//if it matches, don't do anything
+			{/*nothing*/}
 		else
 		{
-			commit(bank, buffer, made);
-			total++;
+			commit(bank, buffer);	//otherwise commit it to the array
 		}
-		if (made == size - 1)
+		if (made == size - 1)		//if the array is at the end, double it
 		{
-			bank = double_array(bank); 
-			doubled++;
+			bank = double_array(bank);	//double_array returns a pointer of wordbank type, so it can be used with the assignment operator
+			doubled++;					//book keeping
 		}
 	}
+}
 
-}*/
-
-wordbank* double_array(wordbank bank[])	//doubles
+wordbank* double_array(wordbank bank[])	//doubles the size, returning an object on pointer
 {
-	wordbank* temp = new wordbank[size*2];
+	wordbank* temp = new wordbank[size*2];	//new temp -> twice the original size
 	for (int i = 0; i < size; i++)
 	{
-		temp[i].word = (&bank[i])->word;
-		temp[i].count = (&bank[i])->count;
+		temp[i].word = bank[i].word;	//reassigns every value
+		temp[i].count = bank[i].count;	//^^
 	}
-	size *= 2;
+	size *= 2;	//book keeping
 
-	return temp;
+	return temp;	//return the ptr/object
 }
 
-bool if_commons(string &buffer)
+bool if_commons(string &buffer)	//this is it's own function largely for readability and personal preference
 {
-	for (int i = 0; i < 50; i++)
+	for (int i = 0; i < 50; i++)	//there are only 50 of them so...
 	{
 		if (buffer.compare(nay.list[i]) == 0)//str.compare kicks the bool only if it's false. Which is weird.
-		{return true;}
+			{return true;}	//the word matches a common word
 	}
-	return false;
+	return false;			//no matches found 
 }
-
 
 void commit(wordbank bank[], string &buffer)
 {
-	bool flag = false;
-	flag = false;
+	bool flag = false;	//'already exists' flag
 	for (int i = 0; i < made; i++)
 	{
-		if (buffer == bank[i].word)
+		if (buffer == bank[i].word)	//if the word is already in the array
 		{
-			bank[i].count++;
-			flag = true;
-			break;
+			bank[i].count++;		//up the counter
+			flag = true;			//set flag to true
+			break;					//break out
 		}
 	}
-	if (flag == false)
+	if (flag == false)				//if no match was found...
 	{
-		bank[made].word = buffer;
-		bank[made].count++;
-		made++;
+		bank[made].word = buffer;	//commit
+		bank[made].count++;			//incriment counter to 1
+		made++;						//book keeping
 	}
-	total++;
+	total++;	//still book keeping
 }
 
 void sort(wordbank bank[])
 {
-	wordbank temp;
-	bool flag = true;
-	while (flag)
+	wordbank temp;		//only need a single object to be stored
+	bool flag = true;	//set a flag and make it true
+	while (flag)		
 	{
-		flag = false;
-		for (int i = 0; i < made-1; i++)
+		flag = false;	//clear(), really
+		for (int i = 0; i < made-1; i++)	//made-1 because it checks for the next entry. Which does not exist at the top
 		{
-			if (bank[i].count < bank[i + 1].count)
+			if (bank[i].count < bank[i + 1].count)	//if current is smaller than the next... swap
 			{
-				flag = true;
-				temp.count = bank[i].count;
-				temp.word = bank[i].word;
-				bank[i].count = bank[i + 1].count;
-				bank[i].word = bank[i + 1].word;
-				bank[i + 1].count = temp.count;
-				bank[i + 1].word = temp.word;
+				flag = true;						//update flag
+				temp.count = bank[i].count;			//store temp variable
+				temp.word = bank[i].word;			//^^
+				bank[i].count = bank[i + 1].count;	//swap
+				bank[i].word = bank[i + 1].word;	//^^
+				bank[i + 1].count = temp.count;		//insert temp back into the array
+				bank[i + 1].word = temp.word;		//^^
 			}
 		}
 	}
